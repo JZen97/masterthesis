8329870ec180b60817cc1c0e388c3b881efb0f1c
Jonas Zender <@lize4033>
2025-05-08 12:00:20 +0200

    Add training script for experiment 1 - learning rate and earthquake epicenter

---
+++ src/training_scripts/exp1_lr_and_epicenter/exp1_lr_and_epicenter.py
@@ -0,0 +1,290 @@
+# Experiment 1) Learning Rate and Earthquake Epicenter
+
+import torch
+import torch.nn as nn
+import torch_geometric
+from tqdm import tqdm
+
+import sys, pathlib, argparse, csv
+
+src_path = 'adjust/this/path'
+sys.path.append(src_path)
+from data.graph_instance import GIDatasetPyG
+
+
+parser = argparse.ArgumentParser(description='Train the model with different learning rates',
+                                 formatter_class=argparse.MetavarTypeHelpFormatter)
+parser.add_argument('--lr', type=float, help='learning rate')
+parser.add_argument('--epochs', type=int, help='number of epochs')
+parser.add_argument('--checkpoint', type=str, default='', help='checkpoint path')
+args = parser.parse_args()
+
+#----------------------------------------------------------------
+# Model Definition
+#----------------------------------------------------------------
+
+class ModelExp1(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+        self.conv1 = nn.Conv1d(in_channels=3, out_channels=32, kernel_size=125, stride=2)
+        self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=125, stride=2)
+
+        # Graph Layers
+        self.gcn1 = torch_geometric.nn.GCNConv(10051, 64, add_self_loops=True, normalize=True, bias=False)
+        self.gcn2 = torch_geometric.nn.GCNConv(64, 64, add_self_loops=True, normalize=True, bias=False)
+
+        # Output Layers
+        self.fc_ims = nn.Linear(64, 5) # PGA, PGV, SA03, SA10, SA30
+        self.fc_epi = nn.Linear(64, 2) # epicenter position
+
+    def forward(self, x1_ts, x2_static, edge_index, edge_weight, batch):
+        x = self.relu(self.conv1(x1_ts)) # out: bs x 32 x 438
+        x = self.relu(self.conv2(x)) # out: bs x 64 x 157
+        # flatten the cnn output
+        x = torch.flatten(x, start_dim=1) # out: bs x 10048
+        # Concatenate output with static features
+        x = torch.concat([x, x2_static], dim=1) # out: bs x 10051
+        # Apply graph layers
+        x = self.relu(self.gcn1(x, edge_index=edge_index, edge_weight=edge_weight)) # out: bs x 64
+        x = self.tanh(self.gcn2(x, edge_index=edge_index, edge_weight=edge_weight)) # out: bs x 64
+        # Apply output layers
+        # Node level predicitons (Intensity Measurements)
+        ims = self.fc_ims(x)
+        # Graph level predictions (Epicenter)
+        x_pooled = torch_geometric.nn.global_mean_pool(x, batch)
+        epi = self.fc_epi(x_pooled)
+        return ims, epi
+
+#----------------------------------------------------------------
+# Training and Evaluation Functions
+#----------------------------------------------------------------
+
+def evaluate_model(model, dl, criterion):
+    model.eval()
+    total_node_loss = 0.
+    total_graph_loss =0.
+    with tqdm(total=len(dl)) as pbar:
+        for idx, batch in enumerate(dl):
+            # get data
+            batch = batch.cuda()
+            x = batch.x
+            x1_ts = x[:, :3000].reshape(-1, 3, 1000) # the first 3000 (3 channels, 10s*100Hz=1000 values/channel) are time series, the rest are static features (position/elevation)
+            x2_static = x[:, 3000:] # positione and elevation
+            edge_index = batch.edge_index # normal adjacency matrix (edges)
+            edge_weight = batch.edge_weight # edge weights
+            pyg_batch = batch.batch # this defines which rows/columns belong to which sample in the batch (as adjacency matrices are put into a block matrix)
+            # forward pass
+            ims, epi = model(x1_ts, x2_static, edge_index, edge_weight, pyg_batch)
+            # get targets
+            y = batch.y # ground truth intensity measurements
+            graph_y = batch.graph_y # epi dists (2), norm_factor (1), reference lat/lon (1)
+            epi_y = graph_y.reshape(dl.batch_size, -1)[:,:2] # only use the epicenter position for loss calculation
+            # compute node and graph level loss
+            node_loss = criterion(ims, torch.log10(y)) # node level loss (Intensity Measurements)
+            graph_loss = criterion(epi, epi_y) # graph level loss (epicenter prediction)
+            # overall loss
+            total_node_loss += node_loss.item()
+            total_graph_loss += graph_loss.item()
+            pbar.set_description(f'Evaluating...')
+            pbar.update(1)
+    return total_node_loss, total_graph_loss
+
+
+def train_model(model, model_save_path, model_name, train_dl, val_dl, epochs, lr, checkpoint_path=None):
+    # load checkpoint if desired
+    if checkpoint_path:
+        checkpoint = torch.load(checkpoint_path)
+        start_epoch = checkpoint['epoch'] + 1
+        model.load_state_dict(checkpoint['model_state_dict'])
+        is_finetuning = 'FT_'
+        print(f'Loaded model parameters from {checkpoint_path}')
+    else:
+        start_epoch = 0
+        print('Initializing model from scratch...')
+        is_finetuning = ''
+    stop_epoch = start_epoch + epochs
+    print(f'Starting training for epochs {start_epoch} to {stop_epoch}...')
+    last_model_checkpoint_path = None
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    criterion = nn.MSELoss()
+    # list of losses (over the epochs) for training and validation split
+    train_node_losses = []
+    train_graph_losses = []
+    train_total_losses = []
+    val_node_losses = []
+    val_graph_losses = []
+    val_total_losses = []
+
+    # Training
+    for epoch in range(start_epoch, stop_epoch):
+        model.train()
+
+        total_loss = 0.0
+        total_node_loss = 0.0
+        total_graph_loss = 0.0
+
+        with tqdm(total=len(train_dl)) as pbar:
+            for idx, batch in enumerate(train_dl):
+                # reset parameter gradients
+                optimizer.zero_grad()
+                # get data
+                batch = batch.cuda()
+                x = batch.x
+                x1_ts = x[:, :3000].reshape(-1, 3, 1000) # the first 3000 (3 channels, 10s*100Hz=1000 values/channel) are time series, the rest are static features (position/elevation)
+                x2_static = x[:, 3000:] # positione and elevation
+                edge_index = batch.edge_index # normal adjacency matrix (edges)
+                edge_weight = batch.edge_weight # edge weights
+                pyg_batch = batch.batch # this defines which rows/columns belong to which sample in the batch (as adjacency matrices are put into a block matrix)
+                # forward pass
+                ims, epi = model(x1_ts, x2_static, edge_index, edge_weight, pyg_batch)
+                # get targets
+                y = batch.y # ground truth intensity measurements
+                graph_y = batch.graph_y # epi dists (2), norm_factor (1), reference lat/lon (1)
+                epi_y = graph_y.reshape(train_dl.batch_size, -1)[:,:2] # only use the epicenter position for loss calculation
+                # compute node and graph level loss
+                node_loss = criterion(ims, torch.log10(y)) # node level loss (Intensity Measurements)
+                graph_loss = criterion(epi, epi_y) # graph level loss (epicenter prediction)
+                loss = node_loss + graph_loss # combined loss
+                # backward pass
+                loss.backward()
+                # optimization step
+                optimizer.step()
+                # running variables
+                total_node_loss += node_loss.item()
+                total_graph_loss += graph_loss.item()
+                total_loss = total_node_loss + total_graph_loss
+                pbar.set_description(f'Epoch {epoch}. Loss: {loss.item()} (Node-loss: {node_loss.item()}, graph-loss: {graph_loss.item()}). Avg. Loss: {total_loss/(idx+1)}')
+                pbar.update(1)
+
+        # save training losses
+        train_node_losses.append(total_node_loss / len(train_dl))
+        train_graph_losses.append(total_graph_loss / len(train_dl))
+        train_total_losses.append(total_loss / len(train_dl))
+
+        # Evaluation
+        val_node_loss, val_graph_loss = evaluate_model(model, val_dl, criterion)
+        val_total_loss = val_node_loss + val_graph_loss
+        val_node_losses.append(val_node_loss / len(val_dl))
+        val_graph_losses.append(val_graph_loss / len(val_dl))
+        val_total_losses.append(val_total_loss / len(val_dl))
+
+        # Plot message
+        print(f'Evaluation: Train Loss (Node): {train_node_losses[-1]} | Val Losses (Node): {val_node_losses[-1]}')
+
+        # check if this is the best model so far:
+        if (val_node_loss / len(val_dl)) <= min(val_node_losses):
+            print('New best validation loss. Saving model...')
+            # save the model state
+            out_path = pathlib.Path(model_save_path) / f'{is_finetuning}{model_name}_epoch{epoch}_val_loss_{val_node_losses[-1]}_best.pt'
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'train_loss': total_node_loss,
+                'val_loss': val_node_loss
+            }, out_path)
+            # remove the previously saved model which performed worse
+            if last_model_checkpoint_path:
+                pathlib.Path(last_model_checkpoint_path).unlink()
+            # update model path
+            last_model_checkpoint_path = str(out_path)
+
+        # Save losses and metrics
+        csv_path = pathlib.Path(model_save_path) / f'{model_name}_losses.csv'
+        if epoch == 0:
+            with open(csv_path, 'w', newline='') as f:
+                writer = csv.writer(f)
+                header = ['epoch', 'train_node_loss', 'train_graph_loss', 'train_total_loss', 'val_node_loss', 'val_graph_loss', 'val_total_loss']
+                # write header
+                writer.writerow(header)
+        # write evaluation results to csv file
+        with open(csv_path, mode='a', newline='') as f:
+            writer = csv.writer(f)
+            row = [epoch, train_node_losses[-1], train_graph_losses[-1], train_total_losses[-1], val_node_losses[-1], val_graph_losses[-1], val_total_losses[-1]]
+            # write line to csv file
+            writer.writerow(row)
+
+    # After the training, save the last model state
+    out_path = pathlib.Path(model_save_path) / f'{is_finetuning}{model_name}_epoch{epoch}_val_loss_{val_node_losses[-1]}_last.pt'
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'train_loss': total_node_loss,
+        'val_loss': val_node_loss
+    }, out_path)
+
+    return model
+
+
+if __name__ == '__main__':
+    #----------------------------------------------------------------
+    # Dataset and Dataloader
+    #----------------------------------------------------------------
+
+    # Paths
+    meta_csv_path = 'GraphInstance/events_meta_data.csv'
+    distances_npy_path = 'GraphInstance/station_distances_km.npy'
+    h5_path = 'GraphInstance/GraphInstance_events_gm.hdf5'
+    train_path = 'GraphInstance/GI_train.txt'
+    val_path = 'GraphInstance/GI_val.txt'
+    test_path = 'GraphInstance/GI_test.txt'
+
+    # Pytorch Geometric Dataset
+    train_ds = GIDatasetPyG(meta_csv_path=meta_csv_path,
+                            distances_npy_path=distances_npy_path,
+                            h5_path=h5_path,
+                            split_path=train_path,
+                            ts_length=10,
+                            edge_cutoff=0.0,
+                            graph_format='complete',
+                            min_node_degree=3,
+                            pad_ts_s=5,
+                            verbose=False)
+
+    val_ds = GIDatasetPyG(meta_csv_path=meta_csv_path,
+                          distances_npy_path=distances_npy_path,
+                          h5_path=h5_path,
+                          split_path=val_path,
+                          ts_length=10,
+                          edge_cutoff=0.0,
+                          graph_format='complete',
+                          min_node_degree=3,
+                          deterministic=True,
+                          pad_ts_s=5,
+                          verbose=False)
+
+    test_ds = GIDatasetPyG(meta_csv_path=meta_csv_path,
+                           distances_npy_path=distances_npy_path,
+                           h5_path=h5_path,
+                           split_path=test_path,
+                           ts_length=10,
+                           edge_cutoff=0.0,
+                           graph_format='complete',
+                           min_node_degree=3,
+                           deterministic=True,
+                           pad_ts_s=5,
+                           verbose=False)
+
+    # Dataloader
+    train_dl = torch_geometric.data.DataLoader(train_ds, batch_size=4, shuffle=True, drop_last=True)
+    val_dl = torch_geometric.data.DataLoader(val_ds, batch_size=4, shuffle=False, drop_last=True)
+    test_dl = torch_geometric.data.DataLoader(test_ds, batch_size=4, shuffle=False, drop_last=True)
+
+
+    #----------------------------------------------------------------
+    # Training
+    #----------------------------------------------------------------
+
+    model_exp_1 = ModelExp1().cuda()
+    epochs = args.epochs
+    lr = args.lr
+    checkpoint = str(args.checkpoint)
+    model_save_path = 'adjust/this/path'
+    model_name = f'ModelExp1_lr{lr}'
+    print(f'Starting Training with Learning rate: {lr} for {epochs} epochs. \n==============================================')
+    model = train_model(model_exp_1, model_save_path, model_name, train_dl, val_dl, epochs, lr, checkpoint_path=checkpoint)
+    print('Finished Training.')
